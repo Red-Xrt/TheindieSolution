d65a6ecdce17c05fefbb7bcb21392d72fd16a0a1
WhiteOnet <laiminhdang123@gmail.com>
2025-11-29 21:25:28 +0700

    Uninstall visual

--- Theindie/Views/Components/GameDetailView.axaml.cs
+++ Theindie/Views/Components/GameDetailView.axaml.cs
@@ -6,9 +6,9 @@ namespace Theindie.Views.Components
 {
     public partial class GameDetailView : UserControl
     {
+        // Các sự kiện giao tiếp với bên ngoài
         public event EventHandler<RoutedEventArgs>? BackRequested;
         public event EventHandler<RoutedEventArgs>? InstallRequested;
-        // Thêm sự kiện Gỡ
         public event EventHandler<RoutedEventArgs>? UninstallRequested;
 
         public GameDetailView()
@@ -16,10 +16,39 @@ namespace Theindie.Views.Components
             InitializeComponent();
         }
 
+        // Logic xử lý nút Back và Install
         private void BackButton_Click(object? sender, RoutedEventArgs e) => BackRequested?.Invoke(this, e);
         private void InstallButton_Click(object? sender, RoutedEventArgs e) => InstallRequested?.Invoke(this, e);
 
-        // Xử lý click Gỡ
-        private void UninstallButton_Click(object? sender, RoutedEventArgs e) => UninstallRequested?.Invoke(this, e);
+        // --- LOGIC HỘP THOẠI GỠ CÀI ĐẶT ---
+
+        // 1. Hiện hộp thoại
+        private void UninstallButton_Click(object? sender, RoutedEventArgs e)
+        {
+            if (UninstallConfirmOverlay != null)
+            {
+                UninstallConfirmOverlay.IsVisible = true;
+            }
+        }
+
+        // 2. Ẩn hộp thoại (Hủy)
+        private void CancelUninstall_Click(object? sender, RoutedEventArgs e)
+        {
+            if (UninstallConfirmOverlay != null)
+            {
+                UninstallConfirmOverlay.IsVisible = false;
+            }
+        }
+
+        // 3. Xác nhận gỡ (Đồng ý)
+        private void ConfirmUninstall_Click(object? sender, RoutedEventArgs e)
+        {
+            if (UninstallConfirmOverlay != null)
+            {
+                UninstallConfirmOverlay.IsVisible = false;
+            }
+            // Gửi tín hiệu ra MainWindow để xử lý logic gỡ
+            UninstallRequested?.Invoke(this, e);
+        }
     }
 }
